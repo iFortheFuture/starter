900c3a34386b9c5506c92153a85ee4f0bd7cba36
陈炜烽 <119088914+iFortheFuture@users.noreply.github.com>
2025-09-02 13:09:02 +0800

    Add files via upload

---
+++ aliyun-oss-spring-boot-autoconfigure/src/main/java/com/aliyun/oss/AliOSSAutoConfiguration.java
@@ -0,0 +1,16 @@
+package com.aliyun.oss;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigurationProperties(AliOSSProperties.class)
+@Configuration
+public class AliOSSAutoConfiguration {
+    @Bean
+    public AliOSSUtils getAliOSSUtils(AliOSSProperties aliOSSProperties) {
+        AliOSSUtils aliOSSUtils = new AliOSSUtils();
+        aliOSSUtils.setAliOSSProperties(aliOSSProperties);
+        return aliOSSUtils;
+    }
+}
